1dd17f1dcac0ee76f6bc3313e88449a72204be1f
Anik <sahaanik1045@gmail.com>
2024-09-07 10:49:11 +0600

    specialist section done

--- src/component/UI/HomePage/Specialist/Specialist.tsx
+++ src/component/UI/HomePage/Specialist/Specialist.tsx
@@ -1,5 +1,6 @@
 import { baseUrl } from "@/assets/url/BaseUrl";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
 const Specialist = async () => {
@@ -29,6 +30,53 @@ const Specialist = async () => {
             Exprienced Doctor Accross All Specialist
           </Typography>
         </Box>
+        <Stack direction="row" gap={4} mt={5}>
+          {specialties.map((specialty: any) => (
+            <Box
+              key={specialty.id}
+              sx={{
+                flex: 1,
+                width: "150px",
+                backgroundColor: "rgba(245, 245, 245,1)",
+                border: "1px solid rgba(250, 250, 250, 1)",
+                borderRadius: "10px",
+                textAlign: "center",
+                padding: "40px 10px",
+                "& img": {
+                  width: "50px",
+                  height: "50px",
+                  margin: "0 auto",
+                },
+                "&:hover": {
+                  border: "1px solid rgba(36, 153, 239, 1)",
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                  transition: "all 0.5s",
+                },
+              }}
+            >
+              <Image
+                src={specialty.icon}
+                width={100}
+                height={100}
+                alt="specialty icon"
+              />
+              <Box>
+                <Typography component="p" fontWeight={600} fontSize={18} mt={2}>
+                  {specialty.title}
+                </Typography>
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Button
+          variant="outlined"
+          sx={{
+            marginTop: "20px",
+          }}
+        >
+          View ALL
+        </Button>
       </Box>
     </Container>
   );
